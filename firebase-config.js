9e6e267e971b7d4786c6c23078d54d6e2fb4fb7e
ChrisLee <ChrisLee@hyenatek.com>
2025-11-05 13:32:21 +0800

    Update firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Firebase 設定檔
+// ⚠️ 請將以下設定替換為你自己的 Firebase 專案設定
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDrMs8VqIJKK9H0lwq24d5uMMPQfimHBIE",
+    authDomain: "taiwan-pool-map.firebaseapp.com",
+    projectId: "taiwan-pool-map",
+    storageBucket: "taiwan-pool-map.firebasestorage.app",
+    messagingSenderId: "169847146397",
+    appId: "1:169847146397:web:aa8ba97c194ecfe31ad174",
+    measurementId: "G-6ZFFQ0KWT9"
+};
+
+// 不要修改以下內容
+if (typeof window !== 'undefined') {
+    window.firebaseConfig = firebaseConfig;
+}
